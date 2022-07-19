d1d9df3cc35b818b684b40c727ebd50500e469dc
Yarisson <gemini8811@gmail.com>
2022-07-19 16:45:40 +0400

    добавление приватного роута

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,11 +1,13 @@
 import {Route, BrowserRouter, Routes} from 'react-router-dom';
+import {AppRoute, AuthorizationStatus} from '../../const';
 import NotFound from '../../pages/404/notFound';
 import Favorites from '../../pages/favorites/favorites';
 import Login from '../../pages/login/login';
-import Property from '../../pages/property/property';
-import {AppRoute} from '../../const';
 import Main from '../../pages/main/main';
+import Hotel from '../../pages/hotel/hotel';
+import PrivateRoute from '../private-route/privateRoute';
 
+const id = 1;
 
 function App(): JSX.Element {
   return (
@@ -17,7 +19,13 @@ function App(): JSX.Element {
         />
         <Route
           path={AppRoute.Favorites}
-          element={<Favorites />}
+          element={
+            <PrivateRoute
+              authorizationStatus={AuthorizationStatus.NoAuth}
+            >
+              <Favorites />
+            </PrivateRoute>
+          }
         />
         <Route
           path={AppRoute.Login}
@@ -25,7 +33,7 @@ function App(): JSX.Element {
         />
         <Route
           path={AppRoute.Room}
-          element={<Property />}
+          element={<Hotel id={id} />}
         />
         <Route
           path={'*'}
