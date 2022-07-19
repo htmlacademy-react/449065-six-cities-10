af4270de062e76a1feb9cc3800933b3baa76444d
Yarisson <gemini8811@gmail.com>
2022-07-19 05:16:34 +0400

    добавление роутов и описание путей на страницы

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,9 +1,38 @@
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
+import NotFound from '../../pages/404/notFound';
+import Favorites from '../../pages/favorites/favorites';
+import Login from '../../pages/login/login';
+import Property from '../../pages/property/property';
+import {AppRoute} from '../../const';
 import Main from '../../pages/main/main';
 
 
 function App(): JSX.Element {
   return (
-    <Main />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={'/'}
+          element={<Main />}
+        />
+        <Route
+          path={AppRoute.Favorites}
+          element={<Favorites />}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<Login />}
+        />
+        <Route
+          path={AppRoute.Room}
+          element={<Property />}
+        />
+        <Route
+          path={'*'}
+          element={<NotFound />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
