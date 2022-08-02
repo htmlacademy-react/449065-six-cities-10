4d012b524b89ef7cf140519be6550efb875bab80
Yarisson <gemini8811@gmail.com>
2022-08-03 01:05:32 +0400

    фикс для хука с картой

--- project/src/hooks/useMap.tsx
+++ project/src/hooks/useMap.tsx
@@ -9,28 +9,35 @@ function useMap(
   const [map, setMap] = useState<Map | null>(null);
 
   useEffect(() => {
-    if (mapRef.current !== null && map === null) {
-      const instance = new Map(mapRef.current, {
-        center: {
-          lat: city.location.latitude,
-          lng: city.location.longitude
-        },
-        zoom: city.location.zoom
-      });
-
-      const layer = new TileLayer(
-        'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
-        {
-          attribution:
-            '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-        }
-      );
-
-      instance.addLayer(layer);
-
-      setMap(instance);
+    if (mapRef.current === null) {
+      return;
     }
-  }, [mapRef, map, city]);
+
+    const instance = new Map(mapRef.current, {
+      center: {
+        lat: city.location.latitude,
+        lng: city.location.longitude
+      },
+      zoom: city.location.zoom
+    });
+
+    const layer = new TileLayer(
+      'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
+      {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+      }
+    );
+
+    instance.addLayer(layer);
+
+    setMap(instance);
+
+    return () => {
+      setMap(null);
+      instance.remove();
+    };
+  }, [mapRef, city.location]);
 
   return map;
 }
